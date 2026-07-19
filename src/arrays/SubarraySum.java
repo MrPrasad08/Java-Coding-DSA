95fe1d4f0d6e5dfb083b35ed19fa14ed7b2912b6
MrPrasad <durgaprasadalb@gmail.com>
2026-07-19 18:43:39 +0530

    Sub Arrays Sum

---
+++ src/arrays/SubarraySum.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class SubarraySum {
+	public void sum(int arr[]) {
+		int n=arr.length;
+		for(int i=0; i<n; i++) {
+			for(int j=i; j<n; j++) {
+				for(int k=i; k<=j; k++) {
+					System.out.print(arr[k]+" ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String args[]) {
+		SubarraySum obj = new SubarraySum();
+		int arr1[] = {1,2,3,4,5,6};
+		obj.sum(arr1);
+	}
+}
