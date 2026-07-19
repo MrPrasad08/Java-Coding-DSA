3daca4ae3a664eaa3e7fa7b787feda3625b9627c
MrPrasad <durgaprasadalb@gmail.com>
2026-07-19 19:01:47 +0530

    Maximum Sum of Sub Arrays

--- src/arrays/SubarraySum.java
+++ src/arrays/SubarraySum.java
@@ -3,19 +3,28 @@ package arrays;
 public class SubarraySum {
 	public void sum(int arr[]) {
 		int n=arr.length;
-		for(int i=0; i<n; i++) {
-			for(int j=i; j<n; j++) {
-				for(int k=i; k<=j; k++) {
-					System.out.print(arr[k]+" ");
+		int max = 0;
+		for(int len=1; len<=n; len++) {
+			System.out.println("Length is : " +len);
+			for(int start=0; start<=n-len; start++) {
+				int sum =0;
+				for(int j=start; j<start+len; j++) {
+					System.out.print(arr[j]+" ");
+					sum += arr[j];
 				}
+				System.out.print(" ==> Sum is : " +sum);
 				System.out.println();
+				if(sum >= max) {
+					max = sum;
+				}
 			}
+			System.out.println("Maximum Sum is : "+max);
 			System.out.println();
 		}
 	}
 	public static void main(String args[]) {
 		SubarraySum obj = new SubarraySum();
-		int arr1[] = {1,2,3,4,5,6};
+		int arr1[] = {1,4,3,7,5,6};
 		obj.sum(arr1);
 	}
 }
