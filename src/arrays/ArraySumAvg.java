986fd31799d14e84ddcb3e6a7e7ff712ac402af7
MrPrasad <durgaprasadalb@gmail.com>
2026-07-21 22:02:02 +0530

    Array Sum and Average

---
+++ src/arrays/ArraySumAvg.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class ArraySumAvg {
+	public static int sumAvg(int arr[]) {
+		int n = arr.length;
+		int sum =0;
+		double avg;
+		for(int i=0; i<n; i++) {
+			sum += arr[i];
+		}
+		avg = (double)sum/n;
+		System.out.println("Sum is : " +sum);
+		System.out.println("Average : " +avg);
+		return 1;
+	}
+	public static void main(String args[]) {
+		int arr1[] = {4,3,6,8,2,1,9,13};
+		sumAvg(arr1);
+	}
+}
