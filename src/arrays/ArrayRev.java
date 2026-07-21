7dfd35c0d8d4c23ff461135393ce735af0d35d4d
MrPrasad <durgaprasadalb@gmail.com>
2026-07-21 21:42:47 +0530

    Array Reverse

---
+++ src/arrays/ArrayRev.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class ArrayRev {
+	public int[] reverse(int arr[]) {
+		int n = arr.length;
+		for(int i=n-1; i>=0; i--) {
+			System.out.print(arr[i] +" ");
+		}
+		return arr;
+	}
+	public static void main(String args[]) {
+		ArrayRev obj = new ArrayRev();
+		int arr1[] = {10,20,30,40,50,60};
+		obj.reverse(arr1);
+	}
+}
