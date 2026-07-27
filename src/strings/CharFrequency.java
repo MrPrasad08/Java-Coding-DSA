c4e6d87e173645b8b814b72bf0997a3f72b87e7b
MrPrasad <durgaprasadalb@gmail.com>
2026-07-27 16:00:06 +0530

    Characters Count

--- src/strings/CharFrequency.java
+++ src/strings/CharFrequency.java
@@ -4,23 +4,24 @@ public class CharFrequency {
 	public void frequency(String str) {
 		for(int i=0; i<str.length(); i++) {
 			int count =1;
-			boolean found = true;
+			boolean find = false;
 			for(int k=0; k<i; k++) {
 				if(str.charAt(i) == str.charAt(k)) {
-					found = false;
+					find = true;
 					break;
 				}
 			}
-				if(!found)
-					continue;
-				for(int j= i+1; j<str.length(); j++) {
-					if(str.charAt(j) == str.charAt(i)) {
-						count++;
-					}
+			if(find)
+				continue;
+			for(int j=i+1; j<str.length(); j++) {
+				if(str.charAt(i) == str.charAt(j)) {
+					count++;
+					break;
 				}
-				System.out.println(str.charAt(i) +" => " +count);
 			}
+			System.out.println(str.charAt(i)+ " => " +count);
 		}
+	}
 	public static void main(String args[]) {
 		CharFrequency obj = new CharFrequency();
 		String s = "hello";
