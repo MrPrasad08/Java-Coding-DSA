51d96e2f34c51469a167bdb2ffcbdeef8b05d458
MrPrasad <durgaprasadalb@gmail.com>
2026-07-27 15:47:18 +0530

    Zeroes to End

---
+++ src/arrays/ZeroesToEnd.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class ZeroesToEnd {
+	public static void main(String args[]) {
+		int arr[] = {0,2,0,1,3,5,0,0,7};
+		int j=0;
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i] != 0) {
+				arr[j] = arr[i];
+				j++;
+			}
+		}
+		while(j<arr.length) {
+			arr[j] = 0;
+			j++;
+		}
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+}
