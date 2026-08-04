4fbe4f0226216bf68a506dcb70009e3a1a32fd1d
MrPrasad <durgaprasadalb@gmail.com>
2026-08-04 15:32:40 +0530

    First non repeating character

---
+++ src/arrays/FirstNonRepeat.java
@@ -0,0 +1,12 @@
+package arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstNonRepeat {
+	public static void main(String[] args) {
+		int arr[] = {1,1,2,2,3,4,4,3,5};
+		Map<Integer, Integer> map = new HashMap<>();
+		
+	}
+}
