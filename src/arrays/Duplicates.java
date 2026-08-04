0507b03024c90812790534489a2a388a9e0d6980
MrPrasad <durgaprasadalb@gmail.com>
2026-08-04 15:33:19 +0530

    Duplicates of the array

--- src/arrays/Duplicates.java
+++ src/arrays/Duplicates.java
@@ -6,10 +6,19 @@ import java.util.HashSet;
 public class Duplicates {
 	public int duplicatesNums(int arr[]) {
 		int n = arr.length;
-		for(int i=0; i<n-1; i++) {
+		for(int i=0; i<n; i++) {
+			boolean found = false;
+			for(int k=0; k<i; k++) {
+				if (arr[k] == arr[i]) {
+					found = true;
+					break;
+				}
+			}
+			if(found)
+				continue;
 			for(int j=i+1; j<n; j++) {
-				if(arr[j] == arr[i]) {
-					System.out.print(arr[j]+" ");
+				if(arr[i] == arr[j]) {
+					System.out.print(arr[i]+" ");
 					break;
 				}
 			}
@@ -18,7 +27,7 @@ public class Duplicates {
 	}
 	public static void main(String args[]) {
 		Duplicates obj = new Duplicates();
-		int arr1[] = {0,2,1,4,2,5,1,5,5};
+		int arr1[] = {0,2,1,4,2,2,5,1,5,5};
 		obj.duplicatesNums(arr1);
 	}
 }
