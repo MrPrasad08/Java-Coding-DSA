809b1d2fd964031cbdb8840f5bd19fccdd74020a
MrPrasad <durgaprasadalb@gmail.com>
2026-08-06 15:53:25 +0530

    Factorial of a number

---
+++ src/numbers/Factorial.java
@@ -0,0 +1,14 @@
+package numbers;
+
+public class Factorial {
+	public static int fact(int n) {
+		if(n==0){
+			return 1;
+		}
+		return n*fact(n-1);
+	}
+	public static void main(String args[]) {
+		int num = 5;
+		System.out.println(fact(num));
+	}
+}
